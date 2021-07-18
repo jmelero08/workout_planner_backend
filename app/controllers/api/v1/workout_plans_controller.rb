42a2a9a607ba8736a243be80e97dfedaf61e11fd
Jose Melero Martinez <jmelero08@gmail.com>
2021-07-18 13:06:15 -0600

    index and create controller and routes

--- app/controllers/api/v1/workout_plans_controller.rb
+++ app/controllers/api/v1/workout_plans_controller.rb
@@ -1,4 +1,22 @@
 class Api::V1::WorkoutPlansController < ApplicationController
 
-    
+    def index
+        workout_plans = WorkoutPlan.all 
+        render json: workout_plans
+    end
+
+    def create
+        workout_plan = WorkoutPlan.new(workout_plan_params)
+        if workout_plan.save
+            render json: workout_plan, status: :accepted
+        else
+            render json: {errors: workout_plan.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def workout_plan_params
+        params.require(:workout_plan).permit(:title, :description, :image_url, :category_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :workout_plans, only: [:index]
+      resources :workout_plans, only: [:index, :create]
     end
   end  
 end
