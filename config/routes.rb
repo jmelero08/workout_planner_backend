c35ec1f379ed75babc6ddbad57f11d3e6e5198e1
Jose Melero Martinez <jmelero08@gmail.com>
2021-07-19 15:12:16 -0600

    fixed cors

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :workout_plans, only: [:index, :create]
+      resources :categories, only: [:index]
     end
   end  
 end
