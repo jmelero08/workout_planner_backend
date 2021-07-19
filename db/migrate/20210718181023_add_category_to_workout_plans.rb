4f3097693e1a3e7d3bfa733ff5b8c0916f82e5e8
Jose Melero Martinez <jmelero08@gmail.com>
2021-07-19 13:49:16 -0600

    fixed schema

--- db/migrate/20210718181023_add_category_to_workout_plans.rb
+++ db/migrate/20210718181023_add_category_to_workout_plans.rb
@@ -1,5 +1,5 @@
 class AddCategoryToWorkoutPlans < ActiveRecord::Migration[6.1]
   def change
-    add_reference :workout_plans, :category, foreign_key: true
+    add_reference :workout_plans, :category, null: false, foreign_key: true
   end
 end
