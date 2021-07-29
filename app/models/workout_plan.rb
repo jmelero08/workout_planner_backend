44bdad204a1960e7693f88b7cff712f5b242bb3c
Jose Melero Martinez <jmelero08@gmail.com>
2021-07-28 18:19:12 -0600

    Revert "added user model"
    This reverts commit 30f50f44c2973272f7f927aa49c683b5c84fdc9e.

--- app/models/workout_plan.rb
+++ app/models/workout_plan.rb
@@ -1,6 +1,5 @@
 class WorkoutPlan < ApplicationRecord
     belongs_to :category
-    belongs_to :user
 
     validates :title, presence: true 
 end
--- db/migrate/20210728225831_add_user_id_to_workout_plans.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToWorkoutPlans < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :workout_plans, :user, null: false, foreign_key: true
-  end
-end
