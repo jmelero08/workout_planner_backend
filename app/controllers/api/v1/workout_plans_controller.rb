1519fb3788017207e0b70fadeadf0317d3a1fad5
Jose Melero <jmelero08@gmail.com>
2021-08-08 16:47:13 -0600

    fixed update bug

--- app/controllers/api/v1/workout_plans_controller.rb
+++ app/controllers/api/v1/workout_plans_controller.rb
@@ -15,6 +15,21 @@ class Api::V1::WorkoutPlansController < ApplicationController
         end
     end
 
+    def show
+        workout_plan = WorkoutPlan.find_by_id(params[:id]) 
+        render json: WorkoutPlanSerializer.new(workout_plan)
+    end
+
+    def update 
+        workout_plan = WorkoutPlan.find_by_id(params[:id])
+        workout_plan.update(workout_plan_params)
+        if workout_plan.save
+            render json: WorkoutPlanSerializer.new(workout_plan), status: :accepted
+        else
+            render json: {errors: workout_plan.errors.full_messages}, status: :unprocessible_entity
+        end
+    end 
+
     private
 
     def workout_plan_params
